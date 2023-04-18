302e7e29a5dc9f6ccf40553f84a6d7d655b72055
une57 <eunji12llll@gmail.com>
2023-04-18 13:54:27 +0900

    STUDY 가장큰수찾기

--- example1.js
+++ example1.js
@@ -1,39 +1,50 @@
 // 특정 문자열 세기
 function countLetter(str) {
-	let count = 0;
+  let count = 0
   for (let i = 0; i < str.length; i++) {
-    if(str[i].toLowerCase() == 'a')
-    count++;
+    if (str[i].toLowerCase() == "a") count++
   }
-  console.log(count);
+  console.log(count)
 }
 
 // 문자열 삽입
 function makeNumber(num) {
-    let sum = 1;
-    for (let i = 2; i <= num; i++) {
-      sum = sum + '-' + i
-    }
-    console.log(sum)
+  let sum = 1
+  for (let i = 2; i <= num; i++) {
+    sum = sum + "-" + i
   }
+  console.log(sum)
+}
 
 // 홀수 문자열
 function makeOdd(num) {
-    let sum = 1;
-    for (let i = 2; i <= num; i++) {
-    if(i%2 == 1){
+  let sum = 1
+  for (let i = 2; i <= num; i++) {
+    if (i % 2 == 1) {
       sum = String(sum) + i
     }
-}
-    console.log(sum)
   }
-
+  console.log(sum)
+}
 
 // 숫자 더하기
-  function sum(num) {
-    let sum = 0;
-    for (let i = 1; i <= num; i++) {
-    sum = sum + i;
-    }
-    console.log(sum);
+function sum(num) {
+  let sum = 0
+  for (let i = 1; i <= num; i++) {
+    sum = sum + i
+  }
+  console.log(sum)
 }
+
+//가장 큰 수 찾기
+function bigNum(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (Number(str[i]) < Number(str[i + 1])) {
+      console.log(str[i + 1])
+    } else {
+      console.log(str[i])
+    }
+  }
+  console.log()
+}
+bigNum(12345)
